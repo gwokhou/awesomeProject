5822e8f7e77d8e258fb409d235c461762b5140b8
ghz54 <akidezhang@outlook.com>
2019-09-19 22:20:31 +0800

    refactor sum

--- main/sum.go
+++ main/sum.go
@@ -7,3 +7,13 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	// make可以在创建切片的时候指定我们需要的长度和容量。
+	sums = make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return
+}
